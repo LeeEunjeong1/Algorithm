02e4f9c1879f549c2da3fc7f88192e26a771fb5a
LeeEunjeong1 <38210019+LeeEunjeong1@users.noreply.github.com>
2020-07-24 17:06:43 +0900

    BeakJoon for문, while문

---
+++ src/Step3_for/N.java
@@ -0,0 +1,20 @@
+package Step3_for;
+
+import java.util.Scanner;
+
+public class N {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int sum = 0;
+		/*
+		for(int i = 1;i<=N;i++) {
+			System.out.println(i);
+		}
+		*/
+		for(int i = N;i>=1;i--) {
+			System.out.println(i);
+		}
+	}
+
+}
