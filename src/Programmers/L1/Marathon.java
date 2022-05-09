c0db6f331b78055a6273326880ba64ca2fbf3e9b
Eunjeong <tbig1019@gmail.com>
2022-05-09 18:12:21 +0900

    해시 - 블로그참고 (해시사용)

---
+++ src/Programmers/L1/Marathon.java
@@ -0,0 +1,36 @@
+package Programmers.L1;
+
+import java.util.HashMap;
+
+public class Marathon {
+    public String solution(String[] participant, String[] completion) {
+        String answer = "";
+        HashMap<String,Integer> mapP = new HashMap<String, Integer>();
+        for(String s:participant){
+            mapP.put(s,mapP.getOrDefault(s, 0)+1); // 참가자 추가
+        }
+        for(String s:completion){
+            mapP.put(s,mapP.get(s)-1); //완주자 하나씩 제거
+        }
+
+        for(String s:mapP.keySet()){
+            if(mapP.get(s)!=0){
+                answer = s;
+                break;
+            }
+
+        }
+
+        return answer;
+    }
+    public static void main(String[] args) {
+        Marathon mObject = new Marathon();
+        String[] participant = {"mislav", "stanko", "mislav", "ana"};
+        String[] completion = {"stanko", "ana", "mislav"};
+        System.out.println(mObject.solution(participant, completion));
+        
+    }
+    
+}
+
+//https://programmers.co.kr/learn/courses/30/lessons/42576/solution_groups?language=java
