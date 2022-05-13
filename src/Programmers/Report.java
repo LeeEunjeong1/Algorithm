c309a6ce1c3357d4fbab6903457901980dca23d6
Eunjeong <tbig1019@gmail.com>
2022-05-13 18:38:38 +0900

    리폿 문제 푸는중

--- src/Programmers/Report.java
+++ src/Programmers/Report.java
@@ -1,20 +1,37 @@
 package Programmers;
 
-import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 
 public class Report {
     public int[] solution(String[] id_list, String[] report, int k) {
-        int[] answer = {};
+        int[] answer = new int[id_list.length];
 
-        HashMap<String,String> reportMap = new HashMap<String,String>(); //리폿 리스트
+        HashMap<String,HashSet<String>> reportMap = new HashMap<String,HashSet<String>>(); //리폿 리스트
+        //정지 메일 받는 사람
 
+        //reportMap 초기화
+        for(int i = 0 ; i< id_list.length;i++){
+            reportMap.put(id_list[i],new HashSet<>());
+
+        }
+
+        //reportMap에 신고 기록하기
         for(int i = 0 ; i<report.length ; i++){
             String reporting = report[i].split(" ")[0]; //리폿 한 친구
             String reported = report[i].split(" ")[1]; // 리폿당한 친구
            
-            reportMap.put(reporting,reported);
+            reportMap.get(reported).add(reporting); //신고받은 친구 / 신고한 친구
+            System.out.println(reportMap);
+        }
 
+        //k번 이상 친고당하면 이용 정지
+        for(int i = 0 ; i<id_list.length;i++){
+            if(reportMap.get(id_list[i]).size() >= k){ 
+                answer[i]=reportMap.get(id_list[i]).size();
+            }else{
+                answer[i]=0;
+            }
         }
 
         return answer;
