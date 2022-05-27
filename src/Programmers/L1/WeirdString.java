d251f91b38043b9aa37fafb6cfc223c07c000bf2
Eunjeong <tbig1019@gmail.com>
2022-05-27 18:16:26 +0900

    trim 사용하여 앞 뒤  공백 제거

--- src/Programmers/L1/WeirdString.java
+++ src/Programmers/L1/WeirdString.java
@@ -7,39 +7,37 @@ import java.util.ArrayList;
 public class WeirdString {
     public String solution(String s) {
         String answer = "";
+
         ArrayList<Integer> blank = new ArrayList<Integer>();
       
 
-
-        for(int i = 0 ; i<s.length();i++){
-            if(s.charAt(i) == ' '){
+        String ss = s.trim();
+        System.out.println(ss+"d");
+        for(int i = 0 ; i<ss.length();i++){
+            if(ss.charAt(i) == ' '){
                 blank.add(i); // 공백의 번지수        
             }      
    
         }
-        if(s.charAt(s.length()-1)!=' '){
-            blank.add(s.length());
-           
-        }else{
-            s = s.substring(0, s.length()-1);
-        }
+        blank.add(ss.length());
+        System.out.println(blank);
 
 
         for(int b : blank){
-            for(int i = 0 ; i<s.length()+1;i++){  
+            for(int i = 0 ; i<ss.length()+1;i++){  
                 if(i == b||blank.get(blank.indexOf(b)) == blank.size()-1){ // 공백을 만난 것
 
            
                     String word = "";
              
                     if(blank.indexOf(b)==0){
-                         word = s.substring(0, i);
-                    }else if(b==s.length()){
-                        word = s.substring(blank.get(blank.indexOf(b)-1)+1, i);
+                         word = ss.substring(0, i);
+                    }else if(b==ss.length()){
+                        word = ss.substring(blank.get(blank.indexOf(b)-1)+1, i);
 
 
                     }else{
-                        word = s.substring(i-(blank.get(blank.indexOf(b)-1))-2, i);
+                        word = ss.substring(i-(blank.get(blank.indexOf(b)-1))-2, i);
                    }
                  System.out.println(word);
                 
@@ -52,10 +50,9 @@ public class WeirdString {
                             answer += Character.toLowerCase(word.charAt(j));
                         }
                     }
-                    if(i < s.length()-1){
+                    if(i < ss.length()-1){
                         answer+=" ";  
-                    }
-                               
+                    }                               
 
                 }
 
