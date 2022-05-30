32609cc880e51d749a9f2eb77fdab5875cabdc0d
Eunjeong <tbig1019@gmail.com>
2022-05-30 18:33:18 +0900

    switch-case 사용시 break;

--- src/Programmers/L2/OpenChat.java
+++ src/Programmers/L2/OpenChat.java
@@ -19,16 +19,33 @@ public class OpenChat {
                  
             String[] recordArr = record[i].split(" "); 
 
-            if(recordArr[0].equals("Enter")){
-                 // 들어왔습니다 + 닉네임 
-                 nickname.put(recordArr[1], recordArr[2]); // uid/닉네임
-                 msg.add(recordArr[1]+"님이 들어왔습니다.");
+            // if(recordArr[0].equals("Enter")){
+            //      // 들어왔습니다 + 닉네임 
+            //      nickname.put(recordArr[1], recordArr[2]); // uid/닉네임
+            //      msg.add(recordArr[1]+"님이 들어왔습니다.");
 
-            }else if(recordArr[0].equals("Leave")){
-                msg.add(recordArr[1]+"님이 나갔습니다.");         
+            // }else if(recordArr[0].equals("Leave")){
+            //     msg.add(recordArr[1]+"님이 나갔습니다.");         
 
-            }else{
-                nickname.put(recordArr[1],recordArr[2]); // uid/닉네임
+            // }else{
+            //     nickname.put(recordArr[1],recordArr[2]); // uid/닉네임
+            // }
+
+            switch(recordArr[0]){
+                case "Enter":{
+                      // 들어왔습니다 + 닉네임 
+                    nickname.put(recordArr[1], recordArr[2]); // uid/닉네임
+                    msg.add(recordArr[1]+"님이 들어왔습니다.");
+                    break;
+                }
+                case "Leave":{
+                    msg.add(recordArr[1]+"님이 나갔습니다.");         
+                    break;
+                }
+                case "Change":{
+                    nickname.put(recordArr[1],recordArr[2]); // uid/닉네임
+                    break;
+                }
             }
         }
         String[] answer = new String[msg.size()];
