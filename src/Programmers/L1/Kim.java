5399aa2bf8ea17c011011d57395b110fc40ac885
Eunjeong <tbig1019@gmail.com>
2022-05-31 17:58:31 +0900

    -

--- src/Programmers/L1/Kim.java
+++ src/Programmers/L1/Kim.java
@@ -5,7 +5,7 @@ public class Kim {
         String answer = "";
         for(int i = 0 ; i<seoul.length ; i++){
             if(seoul[i].equals("Kim")){
-                answer = "김서방은 "+i+"에 있다";
+                answer = "김서방은 "+i+"에 있다"; 
             }
         }
        
