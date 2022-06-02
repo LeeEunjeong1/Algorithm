120d2569908c9680c5346d4712c6211c467b88b3
Eunjeong <tbig1019@gmail.com>
2022-06-02 17:32:43 +0900

    정렬 후 for문 한개로 대체

--- src/Programmers/L2/PhoneNumList.java
+++ src/Programmers/L2/PhoneNumList.java
@@ -1,32 +1,23 @@
 package Programmers.L2;
 
+import java.util.Arrays;
+
 public class PhoneNumList {
     public boolean solution(String[] phone_book) {
         boolean answer = true;
+        Arrays.sort(phone_book);
+
         for(int i = 0 ; i <phone_book.length ; i++){
-            for(int j = 0 ; j<phone_book.length ; j++){
-                if(phone_book[i].length()<=phone_book[j].length()){
-                    if(phone_book[i].equals(phone_book[j].substring(0, phone_book[i].length()))&& phone_book[i] != phone_book[j]){
-                        System.out.println("false" +phone_book[i]);
-                        return false;
-                        // answer = false;
-                        // break;
-    
-                    }else{
-                        System.out.println("true" +phone_book[i]);
-                        answer = true;
-                    }
-                }else{
-                    System.out.println("true" +phone_book[i]);
-                    answer = true;
+            if(phone_book[i].length()<phone_book[i+1].length()){
+                if(phone_book[i].equals(phone_book[i+1].substring(0, phone_book[i].length()))){
+                    return false;
+                    // answer = false;
+                    // break;
                 }
-              
-            }
-          
+            }     
 
         }
         
-        System.out.println("answer "+answer);
         return answer;
 
     }
