dcc7331cb4ea05809dfccfd245d4fb7d82ca0bbf
Eunjeong <tbig1019@gmail.com>
2022-06-02 18:34:55 +0900

    -

---
+++ src/Programmers/L2/Pair.java
@@ -0,0 +1,14 @@
+package Programmers.L2;
+
+public class Pair {
+    public int solution(String s)
+    {
+        int answer = -1;
+
+        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+        System.out.println("Hello Java");
+
+        return answer;
+    }
+    
+}
