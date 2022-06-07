398eebf2493701ba5ea79c1e4429cc88485deee2
Eunjeong <tbig1019@gmail.com>
2022-06-07 18:51:09 +0900

    -

---
+++ src/Programmers/L2/BigNum.java
@@ -0,0 +1,25 @@
+package Programmers.L2;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+// https://junghn.tistory.com/entry/%EC%9E%90%EB%B0%94-int%EB%A5%BC-%EC%9E%90%EB%A6%BF%EC%88%98%EB%B3%84-int-%EB%B0%B0%EC%97%B4%EB%A1%9C-%EB%B6%84%ED%95%A0  (각 자리수 비교)
+// https://programmers.co.kr/learn/courses/30/lessons/42746 (문제 - 정렬 / 가장 큰 수)
+
+public class BigNum {
+    public String solution(int[] numbers) {
+        String answer = "";
+        ArrayList<Integer> numList = new ArrayList<>();
+
+        for(int n : numbers){
+            numList.add(n);
+        }
+        Collections.sort(numList,Collections.reverseOrder());
+
+        for(int i = 0 ; i < numList.size() ; i++){
+            answer += numList.get(i);
+        }
+        return answer;
+    }
+    
+}
