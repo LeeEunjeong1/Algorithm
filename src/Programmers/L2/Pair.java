e35d3e4f18f4de1ed9493083f027310dcdc4e80f
Eunjeong <tbig1019@gmail.com>
2022-06-07 12:34:41 +0900

    효율성테스트 X

--- src/Programmers/L2/Pair.java
+++ src/Programmers/L2/Pair.java
@@ -1,15 +1,38 @@
 package Programmers.L2;
 
+import java.util.ArrayList;
+
+
 // https://programmers.co.kr/learn/courses/30/lessons/12973
+
 public class Pair {
     public int solution(String s)
     {
+        ArrayList<Character> sList = new ArrayList<Character>();
         int answer = -1;
+        for(int i = 0 ; i<s.length();i++){
+            sList.add(s.charAt(i));
+        }
 
-        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-        System.out.println("Hello Java");
+        for(int i = 0 ; i<sList.size() ; i++){
+            System.out.println("i "+i);
+            if(sList.size()>2&&sList.get(i).equals(sList.get(i+1))){
+                System.out.println(sList.remove(i));
+                System.out.println(sList.remove(i));
+                i=0;    
+                continue;         
+            }      
+            System.out.println(sList);
+  
+        }
 
         return answer;
+    }
+    public static void main(String[] args) {
+        Pair pObejct = new Pair();
+        String s = "baabaa";
+        pObejct.solution(s);
+        
     }
     
 }
