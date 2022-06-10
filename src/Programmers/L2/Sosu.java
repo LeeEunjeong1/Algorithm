c3b21f10086cd86eb336e3bbabc91876e1160da9
Eunjeong <tbig1019@gmail.com>
2022-06-10 17:36:04 +0900

    완전탐색 - 소수찾기

--- src/Programmers/L2/Sosu.java
+++ src/Programmers/L2/Sosu.java
@@ -1,52 +1,35 @@
 package Programmers.L2;
 
-import java.util.ArrayList;
+import java.util.*;
 
 // 완전탐색 - 소수찾기 
 
 public class Sosu {
-
-    ArrayList<Character> numList = new ArrayList<>(); //numbers String -> List
-    ArrayList<Integer> answerList = new ArrayList<>(); //answer List
-    Boolean[] visited;
-
     public int solution(String numbers) {
+        Set<Integer> set = new HashSet<>();
         int answer = 0;
-
-        for(int i = 0 ; i < numbers.length() ; i++){
-            System.out.println(numbers.charAt(i));
-            numList.get(numbers.charAt(i)); // 리스트에 저장 
-        }
-        dfs("",0); // 조합할 숫자 리스트, 숫자 리스트 사이즈
-
-        answer = answerList.size();
-        System.out.println(answerList);
         
+        //조합 재귀함수
+        dfs(numbers,"",set);     
+
+        //소수판별
+        for(Integer num : set){
+            if(isPrime(num)){
+                answer++;
+            }
+        }
+                
         return answer;
     }
 
     // dfs 재귀 - 숫자 조합 후 소수면 set에 추가
-    public void dfs(String num,int cnt){
-        // 탈출조건
-        if(cnt == numList.size() ) return; // numList.size()만큼 다 돌았다
-        // 숫자 조합
-        for(int i = numList.size()-cnt ; i<numList.size();i++){
-            if(visited[i]){
-                continue; //방문한 적 있으면 넘어간다
-            }
-            visited[i] = true;
-            dfs(num+numList.get(i),cnt+1);
-            visited[i]=false;
+    public void dfs(String numbers, String str, Set<Integer> set){
+        if(!str.equals("")){
+            set.add(Integer.valueOf(str));
         }
-        // 소수찾기
-
-        int numInt;
-        if(num != ""){
-            numInt = Integer.parseInt(num);
-            if(isPrime(numInt)){      // 소수면 list에 저장
-                answerList.add(numInt);
-            }
-        }  
+        for(int i = 0 ; i<numbers.length();i++){
+            dfs(numbers.substring(0, i)+numbers.substring(i+1,numbers.length()),str+numbers.charAt(i),set);
+        }
     }
 
     // 소수 찾기
