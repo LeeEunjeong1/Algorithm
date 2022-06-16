45298cd0c28320a159fc7ef32625aaa9ea545087
Eunjeong <tbig1019@gmail.com>
2022-06-16 13:25:16 +0900

    (A + B) % C의 값은 ( ( A % C ) + ( B % C) ) % C와 같다

--- src/Programmers/L2/Fibo.java
+++ src/Programmers/L2/Fibo.java
@@ -6,16 +6,24 @@ package Programmers.L2;
 
 // F(2) = F(0) + F(1) = 0 + 1 = 1
 // F(3) = F(1) + F(2) = 1 + 1 = 2
-// F(4) = F(2) + F(3) = 1 + 2 = 3
-// F(5) = F(3) + F(4) = 2 + 3 = 5
+// F(4) = F(2) + F(3) = 1 + 2 = 3 -> 1+1+1
+// F(5) = F(3) + F(4) = 2 + 3 = 5 -> 1+1+1+1+1
+// F(6) = F(4) + F(5) = 3 + 5 = 8 - > 1+1+1+1+1+1+1+1
 
 public class Fibo {
     public int solution(int n) {
         int answer = 0;
+        int[] numList = new int[n+1];
+        numList[0] = 0;
+        numList[1] = 1;
+        numList[2] = 1;
         
         //answer = fibo(n); 
-
         
+        for(int i = 3 ; i <n+1 ; i++){
+            numList[i] = (numList[i-1]+numList[i-2])%1234567;
+        }
+        answer = numList[n]%1234567;
          
         return answer;
     }
@@ -25,9 +33,11 @@ public class Fibo {
         if(n==0) return 0;
         if(n==1) return 1;
         return fibo(n-1)+fibo(n-2);
+    }
 
-        //재귀함수를 사용하면 시간초과&런타임 에러 -> 깊이 초과
-
+    public static void main(String[] args) {
+        Fibo fObject = new Fibo();
+        fObject.solution(5);
     }
     
 }
